0a0c57e07e1623ddf2da00b1ea7aad7a3d7423b1
xiaomei <testcomt/users.noreply.github.com>
2017-08-15 21:53:23 +0800

    add state transitions: timing

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_IMAGE_TITLE = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR =  '[data-image-role="trigger"]';
 var HIDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
 function setDetails(imageUrl,titleText){
@@ -53,7 +55,13 @@ function hideDetails(){
 
 function showDetails(){
   'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function(){
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  },50);
+
 }
 
 function addKeyPressHandler(){
